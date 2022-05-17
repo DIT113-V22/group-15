e5a0ee80d4b546a5303533c0bbc80cab822d860a
jonnajoh <jonnajoh@net.chalmers.se>
2022-05-17 14:49:28 +0200

    Implemented login function and car talk function. Made changes to pass MeaLinter tests.

--- Website/script.js
+++ Website/script.js
@@ -55,8 +55,11 @@ function search_item() {
     input = input.toLowerCase();
     let x = document.querySelector('#result');
     x.innerHTML = ""
+    var msg = new SpeechSynthesisUtterance();
+    msg.lang = 'en-US';
+    msg.rate = 0.75;
 
-    for (i = 0; i < itemData.length; i++) {
+    for (let i = 0; i < itemData.length; i++) {
         let obj = itemData[i];
 
         if (obj.Item.toLowerCase().includes(input)) {
@@ -64,124 +67,10 @@ function search_item() {
             elem.innerHTML = `${obj.Item} - Quantity: ${obj.Quantity} - Price: ${obj.Price} | Location:  Aisle: ${obj.Aisle} - Section: ${obj.Section} `
             x.appendChild(elem)
 
-            var msg = new SpeechSynthesisUtterance();
-            msg.lang = 'en-US';
-            msg.rate = 0.75;
+
             msg.text = `you can find the ${obj.Item} in Aisle ${obj.Aisle} Section ${obj.Section} at this moment we have ${obj.Quantity} ${obj.Item} in stock and the price is ${obj.Price}`
             window.speechSynthesis.speak(msg);
         }
 
     }
-}
-
-//--------------- Predefined path --------
-/* //not in use anymore, scope changed.
-function addRows(){
-    const table = document.getElementById('myTable');
-    const rowCount = table.rows.length;
-    const cellCount = table.rows[0].cells.length;
-    const row = table.insertRow(rowCount);
-
-    for(var i =0; i <= cellCount; i++){
-        let cell = 'cell'+i;
-        cell = row.insertCell(i);
-        cell.innerHTML=document.getElementById('column' + i).innerHTML;
-
-
-    }
-}
-// not in use
-
-function deleteRows(){
-    const table = document.getElementById('myTable');
-    let rowCount = table.rows.length;
-    if(rowCount > '2'){
-        const row = table.deleteRow(rowCount-1);
-        rowCount--;
-    }
-    else{
-        alert('There should be at least one stage');
-    }
-}
-
-// not in use
-function submit(){
-        const speed = Number.parseInt(document.getElementById("speed").value, 10);
-        const distance = Number.parseInt(document.getElementById("distance").value, 10);
-        const degrees = Number.parseInt(document.getElementById("degrees").value, 10);
-
-        const data = {speed, distance, degrees};
-
-    postData('http://localhost:', data)
-        .then(response => {
-            console.log(response);
-        });
-
-}
-// not in use
-
-async function postData(url = '', data = {}) {
-    const response = await fetch(url, {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
-    return response.json();
-} */
-/// ------------
-
-const mqtt = require('mqtt')
-
-//const host = 'localhost-connect'
-const host = 'broker.emqx.io'
-const port = '1883'
-//
-const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
-//IP
-const connectUrl = `mqtt://${'184.72.144.205'}:${'1883'}`
-
-const client = mqtt.connect(connectUrl, {
-    clientId,
-    clean: true,
-    connectTimeout: 4000,
-    username: 'emqx',
-    password: 'public',
-    reconnectPeriod: 1000,
-})
-//sub
-const topic = '/nodejs/mqtt'
-client.on('connect', () => {
-    console.log('Connected')
-    client.subscribe([topic], () => {
-        console.log(`Subscribe to topic '${topic}'`)
-    })
-})
-
-client.on('message', (topic, payload) => {
-    console.log('Received Message:', topic, payload.toString())
-})
-//pub
-client.on('connect', () => {
-    client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
-        if (error) {
-            console.error(error)
-        }
-    })
-})
-//test
-client.on('connect', () => {
-    client.publish(topic, 'sup Carl, you are connected? maybe i dont know', { qos: 0, retain: false }, (error) => {
-        if (error) {
-            console.error(error)
-        }
-    })
-})
-
-function test () {
-    var msg = new SpeechSynthesisUtterance();
-    msg.text = "Hello World";
-    window.speechSynthesis.speak(msg);
 }
