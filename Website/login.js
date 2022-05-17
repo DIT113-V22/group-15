6e76c8b506252eef7cd3de7f076bf6dd777431c9
jonnajoh <jonnajoh@net.chalmers.se>
2022-05-17 14:46:40 +0200

    Implemented login function and car talk function. Made changes to pass MeaLinter tests.

--- Website/login.js
+++ Website/login.js
@@ -2,11 +2,11 @@
 function getInfo(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-
-        if((username == "admin") && password=="123"){
+    let message = "Invalid username or password"
+        if((username === "admin") && password==="123"){
             window.location = "MovementControlWeb.html";
         }else {
-            alert("Invalid username or password");
+            alert(message);
         }
 
     }
