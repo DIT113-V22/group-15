2fbe1ab687b7c4e36da32dca205cc212b8b02a67
Alaaht <gustalebal@student.gu.se>
2022-05-18 13:56:09 +0200

    updating the smartcar.io sketch - Separating the exacting of the different driving modes - Adding three predefined paths that could use in certain areas of the store - Adding an obstacle avoidance  method that get execute only when having one of the paths activated

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -23,8 +23,21 @@ const auto backIR_PIN = 3;
 const auto maxDistance = 400;
 
 
-bool obstacle = false;
 unsigned int angleToTurn =            1;
+
+
+bool driveMode = false;
+
+bool pathMode1 = false;
+
+bool pathMode2 = false;
+
+bool pathMode3 = false;
+
+volatile int counter;
+
+
+unsigned int delayTime =           20UL;
 
 
 
@@ -32,8 +45,8 @@ unsigned int angleToTurn =            1;
 const char ssid[] =                 "***";
 const char pass[] =                "****";
 
-const auto carSpeed =                50;
-const int SPEED_INCREMENT =          20;
+const auto carSpeed =                70;
+const int SPEED_INCREMENT =          40;
 
 
 
@@ -84,7 +97,7 @@ void setup() {
   mqtt.begin(net);
 #endif
 
-  
+
   Serial.println("Connecting to WiFi...");
   auto wifiStatus = WiFi.status();
   while (wifiStatus != WL_CONNECTED && wifiStatus != WL_NO_SHIELD) {
@@ -101,26 +114,62 @@ void setup() {
     delay(1000);
   }
 
-  
+
   if (mqtt.connect("arduino", "public", "public")) {
     mqtt.subscribe("/smartcar/control/#", 1);
     mqtt.onMessage([](String topic, String message) {
       if (topic == "/smartcar/control/throttle") {
-
         car.setSpeed(message.toInt());
+
       } else if (topic == "/smartcar/control/steering") {
 
         car.setAngle(message.toInt());
-      } else if ( topic == "/smartcar/control/autoDrive") {
+      } else if ( topic == "/smartcar/control/DriveMode") {
         if (message.toInt() == 1) {
-          car.setSpeed(50);
-          autoDrive();
+          
+          driveMode = true;
+          pathMode1 = false;
+          pathMode2 = false;
+          pathMode3 = false;
+          car.setSpeed(carSpeed);
+          aimlessDriveControl();
+          
         }
-      }
-      else if ( topic == "/smartcar/control/ManuellDrive") {
-        if (message.toInt() == 1) {
+        else if (message.toInt() == 0) {
+          
+          driveMode = false;
+          pathMode1 = false;
+          pathMode2 = false;
+          pathMode3 = false;
           manualControl();
+          
+        } else if (message.toInt() == 2)
+        {
+          driveMode = false;
+          pathMode1 = true;
+          pathMode2 = false;
+          pathMode3 = false;
+          path01();
+
+        } else if (message.toInt() == 3)
+        {
+          driveMode = false;
+          pathMode1 = false;
+          pathMode2 = true;
+          pathMode3 = false;
+          path02();
+
+        }else if (message.toInt() == 3)
+        {
+          driveMode = false;
+          pathMode1 = false;
+          pathMode2 = false;
+          pathMode3 = true;
+          path03();
+
         }
+
+
       }
       else {
         Serial.println(topic + " " + message);
@@ -129,11 +178,39 @@ void setup() {
   }
 }
 
+
 void loop() {
 
-  autoDrive();
-  manualControl();
-  detectObstacle();
+  if (driveMode == true && pathMode1 == false && pathMode2 == false && pathMode3 == false )
+  {
+    aimlessDriveControl();
+  }
+  else if (driveMode == false  && pathMode1 == false && pathMode2 == false && pathMode3 == false)
+  {
+    manualControl();
+  }
+
+  else if (driveMode == false  && pathMode1 == true && pathMode2 == false && pathMode3 == false)
+  {
+    path01();
+    detectObstacle();
+    
+  } else if (driveMode == false  && pathMode1 == false && pathMode2 == true && pathMode3 == false)
+  {
+    path02();
+    detectObstacle();
+
+  }
+  else if (driveMode == false  && pathMode1 == false && pathMode2 == false && pathMode3 == true)
+  {
+    path02();
+    detectObstacle();
+
+  }
+
+
+
+
 
 
   if (mqtt.connected()) {
@@ -164,7 +241,7 @@ void loop() {
 
 void manualControl()
 { // handle serial input if there is any
-  obstacle = false;
+
 
 
   if (Serial.available())
@@ -199,43 +276,30 @@ void manualControl()
 
 
 
-void detectObstacle()
+void aimlessDriveControl()
 {
   auto distance_front_IR = FrontIR.getDistance();
   auto US_front_distance = US_front.getDistance();
 
-  
+
   if (US_front_distance > 100 && US_front_distance < 200 || distance_front_IR > 100 && distance_front_IR < 125)
   {
     Serial.print("Obstacle has been detected");
     car.setSpeed(carSpeed - SPEED_INCREMENT);
   }
 
-  else if (US_front_distance > 0 && US_front_distance < 100 && distance_front_IR > 0 && distance_front_IR < 100)
+  else if (US_front_distance > 0 && US_front_distance <= 100 || distance_front_IR > 0 && distance_front_IR <= 100)
   {
-    
-    obstacle = true;
-  }
-  else
-  {
-    car.setSpeed(carSpeed); 
-  }
-}
 
-
-
-void autoDrive()
-{
-  if(obstacle == true)
-  {
+    car.setSpeed(0);
     static long previousTransmission = 0UL;
     long currentTime = millis();
 
     const auto oneSecond = 1000UL;
 
-    car.setSpeed(0);
+
     Serial.println(currentTime);
-    if (currentTime - previousTransmission >= oneSecond * 20)
+    if (currentTime - previousTransmission >= oneSecond * delayTime)
     {
 
       car.setSpeed(-50);
@@ -249,14 +313,220 @@ void autoDrive()
     }
   }
 
-
-
-
+  else
+  {
+    car.setSpeed(carSpeed);
+  }
 }
 
-void randomAngle() //Generate a random angle between 1 and 180 degree each time try to avoid an obstacle
+
+void randomAngle() //Generate a random angle between 1 and 180 degrees each time to try to avoid an obstacle
 {
   int angle = angleToTurn * rand() % 180;
   car.setAngle(angle);
 
+}
+
+
+void costumerDelay()    //Publish to a button on the website and when then button is pressed
+                        // the car gives a 1-minute delay so the customer can interact with the car.
+{
+  car.setSpeed(70);
+  static long previousTransmission = 0UL;
+  long currentTime = millis();
+  const auto oneSecond = 1000UL;
+  Serial.println(currentTime);
+  if (currentTime - previousTransmission >= oneSecond * 60)
+  {
+    previousTransmission = currentTime;
+    currentTime = 0UL;
+    car.setSpeed(100);
+  }
+}
+
+
+
+bool freeToGo = false;
+unsigned int firstInterval = 4000UL;
+
+
+void path01()
+{
+  pathMode1 = true;
+  pathMode2 = false;
+  pathMode3 = false;
+  freeToGo = true;
+  auto distance_front_IR = FrontIR.getDistance();
+  auto US_front_distance = US_front.getDistance();
+if (detectObstacle && freeToGo)
+  {
+  car.setSpeed(50);
+  detectObstacle();
+  delay(3000);
+  detectObstacle();
+  car.setAngle(60);
+  detectObstacle();
+  delay(2000);
+  detectObstacle();
+  car.setAngle(0);
+  detectObstacle();
+  car.setSpeed(60);
+  detectObstacle();
+  delay(3000);
+  detectObstacle();
+  car.setAngle(60);
+  detectObstacle();
+  delay(2000);
+  detectObstacle();
+  car.setAngle(0);
+  detectObstacle();
+  car.setSpeed(50);
+  detectObstacle();
+  delay(3000);
+  detectObstacle();
+  car.setAngle(60);
+  detectObstacle();
+  delay(2000);
+  detectObstacle();
+  car.setAngle(0);
+  }else 
+  {
+    car.setSpeed(0);
+  }
+
+}
+
+
+
+void path03()
+{
+  pathMode1 = false;
+  pathMode2 = true;
+  pathMode3 = false;
+  freeToGo = true;
+
+  auto distance_front_IR = FrontIR.getDistance();
+  auto US_front_distance = US_front.getDistance();
+
+  if (detectObstacle && freeToGo)
+  {
+    car.setSpeed(50);
+    detectObstacle();
+    delay(4000);
+    detectObstacle();
+    car.setAngle(90);
+    detectObstacle();
+    delay(1500);
+    detectObstacle();
+    car.setAngle(0);
+    detectObstacle();
+    car.setSpeed(50);
+    detectObstacle();
+    delay(1500);
+    detectObstacle();
+    car.setAngle(90);
+    detectObstacle();
+    delay(1000);
+    detectObstacle();
+    car.setAngle(0);
+    detectObstacle();
+    car.setSpeed(50);
+    detectObstacle();
+    delay(4000);
+    detectObstacle();
+    car.setAngle(-90);
+    detectObstacle();
+    delay(1000);
+    detectObstacle();
+    car.setAngle(0);
+    detectObstacle();
+    car.setSpeed(50);
+    detectObstacle();
+    delay(1500);
+    detectObstacle();
+    car.setAngle(-90);
+    detectObstacle();
+    delay(1000);
+    detectObstacle();
+    car.setAngle(0);
+    detectObstacle();
+    car.setSpeed(50);
+    detectObstacle();
+    delay(4000);
+    detectObstacle();
+    car.setAngle(-180);
+    detectObstacle();
+    delay(3000);
+    detectObstacle();
+    car.setAngle(0);
+    detectObstacle();
+  }else 
+  {
+    car.setSpeed(0);
+  }
+
+}
+
+void path02()
+{
+  pathMode1 = false;
+  pathMode2 = false;
+  pathMode3 = true;
+  freeToGo = true;
+  auto distance_front_IR = FrontIR.getDistance();
+  auto US_front_distance = US_front.getDistance();
+
+  if (detectObstacle && freeToGo)
+  {
+  car.setSpeed(50);
+  detectObstacle();
+  delay(4000);
+  detectObstacle();
+  car.setAngle(90);
+  detectObstacle();
+  delay(1000);
+  detectObstacle();
+  car.setAngle(0);
+  detectObstacle();
+  car.setSpeed(50);
+  detectObstacle();
+  delay(4000);
+  detectObstacle();
+  car.setAngle(90);
+  detectObstacle();
+  delay(1000);
+  detectObstacle();
+  car.setAngle(0);
+  detectObstacle();
+  }else
+  {
+    car.setSpeed(0);
+  }
+}
+
+
+
+
+
+bool detectObstacle()  //To detect obstacle and stop the car when facing an obstacle.
+                       //We assume that the obstacle is a customer in this case,
+                       //since the path is designed to make the car roam freely in an obstacle-free environment.
+                     
+{
+  auto distance_front_IR = FrontIR.getDistance();
+  auto US_front_distance = US_front.getDistance();
+
+  if (US_front_distance > 50 && US_front_distance <= 100 || distance_front_IR > 50 && distance_front_IR <= 100)
+  {
+    car.setSpeed(30);
+    
+  }
+  if (US_front_distance > 0 && US_front_distance <= 50 || distance_front_IR > 0 && distance_front_IR <= 50)
+  {
+    freeToGo = false;
+    Serial.println("An obstacle is found!!!");
+    car.setSpeed(0);
+  }
+
+  return false;
 }
